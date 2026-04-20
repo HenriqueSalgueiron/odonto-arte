4148dbcf7b47cbe39251a7926fa101f686b3822c
Henrique <henriquehssjc@gmail.com>
2026-04-19 23:14:09 -0300

    fix(server): update host to "::" for app.listen

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -6,7 +6,7 @@ async function start() {
   const app = await buildApp();
 
   try {
-    await app.listen({ port: env.PORT, host: env.HOST });
+    await app.listen({ port: env.PORT, host: "::" });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
